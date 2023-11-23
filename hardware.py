2cf8a2f272f7f3b2c404d9aaccbdcaabb7481c39
huise0ng <shinhuiseong07@gmail.com>
2023-11-24 01:17:53 +0900

    FIX :: Wemos D1 R1으로 보드 변경

--- hardware.py
+++ hardware.py
@@ -1,71 +1,46 @@
 #include <Wire.h>
-#include <SPI.h>
 #include <Adafruit_PN532.h>
 
-#define PN532_SCK  (2)
-#define PN532_MOSI (3)
-#define PN532_SS   (4)
-#define PN532_MISO (5)
+#define SCK  D2
+#define MOSI D3
+#define SS   D4
+#define MISO D5
 
-#define PN532_IRQ   (2)
-#define PN532_RESET (3)
-Adafruit_PN532 nfc(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
-
-
-#if defined(ARDUINO_ARCH_SAMD)
-   #define Serial SerialUSB
-#endif
+Adafruit_PN532 nfc(SCK, MISO, MOSI, SS);
 
 void setup(void) {
-  #ifndef ESP8266
-    while (!Serial); 
-  #endif
   Serial.begin(115200);
   Serial.println("Hello!");
 
   nfc.begin();
 
   uint32_t versiondata = nfc.getFirmwareVersion();
-  if (! versiondata) {
+  if (!versiondata) {
     Serial.print("Didn't find PN53x board");
-    while (1); // halt
+    while (1); 
   }
-
-  Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX); 
-  Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
-  Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
   
-
   nfc.SAMConfig();
-  
-  Serial.println("Waiting for an ISO14443A Card ...");
+  Serial.println("Waiting for an NFC card...");
 }
 
 void loop(void) {
   uint8_t success;
-  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 }; 
-  uint8_t uidLength;                        
-    
-  
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  
+  uint8_t uidLength;                      
+
   success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
-  
+
   if (success) {
-   
-    Serial.println("Found an ISO14443A card");
-    Serial.print("  UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
-    Serial.print("  UID Value: ");
-    nfc.PrintHex(uid, uidLength);
+    Serial.println("Found an NFC card!");
+
+    Serial.print("UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
+    Serial.print("UID Value: ");
+    for (uint8_t i=0; i < uidLength; i++) {
+      Serial.print(" 0x");Serial.print(uid[i], HEX);
+    }
     Serial.println("");
     
-    if (uidLength == 4)
-    {
-      
-      Serial.println("Seems to be a Mifare Classic card (4 byte UID)");
-	  
-     
-      Serial.println("Trying to authenticate block 4 with default KEYA value");
-      uint8_t keya[6] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF }; 
-
-    } 
-  } 
+    delay(1000);
+  }
 }
